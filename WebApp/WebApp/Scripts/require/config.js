42238cfa26a1a384fab7ab2e73d9c69b0f2ff6fa
HowardRichards <dev@conficient.com>
2014-11-03 11:50:32 +0000

    Added config.js and map file to tracking

---
+++ WebApp/WebApp/Scripts/require/config.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../typings/requirejs/require.d.ts" />
+require.config({
+    baseUrl: "/Scripts/",
+    paths: {
+        jquery: "jquery-2.1.1.min",
+        bootstrap: "bootstrap.min",
+        knockout: "knockout-3.2.0"
+    },
+    shim: {
+        "bootstrap": ["jquery"]
+    }
+});
+//# sourceMappingURL=config.js.map
